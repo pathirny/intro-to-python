1a4892bb0efdf90bd2ca2cb63f2902b9e25a4eeb
Patryk Hirny <pathirny@googlemail.com>
2024-04-09 13:11:17 +0100

    mapping

--- 035_mapping.py
+++ 035_mapping.py
@@ -35,7 +35,11 @@ print("Function: add_one_hundred_to_numbers")
 
 # Return a new list of each number with 100 added
 def add_one_hundred_to_numbers(numbers):
-  pass
+  #acumulator
+  total = []
+  for number in numbers:
+    total.append(number + 100)
+  return total
 
 check_that_these_are_equal(
   add_one_hundred_to_numbers([1, 2, 3, 4]), [101, 102, 103, 104])
